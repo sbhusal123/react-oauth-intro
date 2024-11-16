eee47661c9920ccef55191c0e113ac7278d47c9d
sbhusal123 <suryabhusal11@gmail.com>
2024-11-17 01:27:43 +0545

    Login, registration for user.

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ import { FACEBOOK_APP_ID } from './config'
 import AuthService from './services/auth';
 
 function App() {
-  const handleGoogleLoginSuccess = async (credentialResponse) => {
+  const handleGoogleRegister = async (credentialResponse) => {
     try {
       console.log("credentialResponse:: ", credentialResponse);
 
       // Send the ID token to the Django backend for validation and login:
       // Note that, the token obtained is only useful for single request
-      const response = await AuthService.googleAuth(credentialResponse.code);
+      const response = await AuthService.googleRegister(credentialResponse.code);
       console.log('Login/Registration successful:', response.data);
       // You can now use the response to store the token or update the UI
 
@@ -25,18 +25,40 @@ function App() {
     }
   };
 
+  const handleGoogleLogin = async (credentialResponse) => {
+    try {
+      console.log("credentialResponse:: ", credentialResponse);
+
+      // Send the ID token to the Django backend for validation and login:
+      // Note that, the token obtained is only useful for single request
+      const response = await AuthService.googleLogin(credentialResponse.code);
+      console.log('Login/Registration successful:', response.data);
+      // You can now use the response to store the token or update the UI
+
+    } catch (error) {
+      console.error('Login Failed:', error);
+      alert('Login failed. Please try again.');
+    }
+  };  
+
   const handleGoogleLoginError = () => {
     console.log('Login Failed');
     alert('Login failed. Please try again.');
   };
 
-  const googleLogin = useGoogleLogin({
-    onSuccess: handleGoogleLoginSuccess,
+  const googleRegister = useGoogleLogin({
+    onSuccess: handleGoogleRegister,
     onError: handleGoogleLoginError,
     flow: 'auth-code'
   })
 
-  const responseFacebook = async (response) => {
+  const googleLogin = useGoogleLogin({
+    onSuccess: handleGoogleLogin,
+    onError: handleGoogleLoginError,
+    flow: 'auth-code'
+  })  
+
+  const handleFacebookRegister = async (response) => {
     if (response.status !== 'unknown') {
       try {
         console.log("Facebook Oauth Consent response:", response)
@@ -46,7 +68,7 @@ function App() {
         // Handle login success
   
         // Send the ID token to the Django backend for validation and login
-        const authResp = await AuthService.facebookAuth(decode.code);
+        const authResp = await AuthService.facebookRegister(decode.code);
         console.log('Login/Registration successful:', authResp.data);
   
         console.log("Decoded Signed Request::", decode)
@@ -61,18 +83,37 @@ function App() {
     }
   };  
 
+  const handleFacebookLogin = async (response) => {
+    if (response.status !== 'unknown') {
+      try {
+        console.log("Facebook Oauth Consent response:", response)
+
+        const decode = jwtDecode(response.signedRequest)
+        console.log("Decoded Signed :", decode)
+        // Handle login success
+  
+        // Send the ID token to the Django backend for validation and login
+        const authResp = await AuthService.facebookLogin(decode.code);
+        console.log('Login/Registration successful:', authResp.data);
+  
+        console.log("Decoded Signed Request::", decode)
+      } catch (error) {
+        console.log("Login error::", error)
+        alert('Login failed. Please try again.');
+      }
+
+    } else {
+      // Handle login failure
+      console.error('Facebook login failed:', response);
+    }
+  };    
+
   return (
     <div className="App">
       {}
       <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button onClick={googleLogin}>
-            Login In Google
+          <button onClick={googleRegister}>
+            Register With Google
           </button>
           <br/>
           <br/>
@@ -80,13 +121,31 @@ function App() {
           <FacebookLogin
             appId={FACEBOOK_APP_ID} // Replace with your Facebook App ID
             autoLoad={false} // Whether to automatically load the login dialog
-            callback={responseFacebook}
+            callback={handleFacebookRegister}
             icon="fa-facebook"
+            textButton='Register With Facebook'
             scope=''
             size='small'
             responseType='code'
           />
-        </a>
+          <br/>
+          <br/>
+          <br/>
+          <button onClick={googleLogin}>
+            Login With Google
+          </button>
+          <br/>
+          <br/>
+          <FacebookLogin
+            appId={FACEBOOK_APP_ID} // Replace with your Facebook App ID
+            autoLoad={false} // Whether to automatically load the login dialog
+            callback={handleFacebookLogin}
+            icon="fa-facebook"
+            textButton='Login With Facebook'
+            scope=''
+            size='small'
+            responseType='code'
+          />
       </header>
     </div>
   );
--- src/services/auth.js
+++ src/services/auth.js
@@ -2,15 +2,27 @@ import api from "./index"
 
 export default class AuthService {
 
-    static googleAuth(code) {
+    static googleRegister(code) {
         return api.post('/auth/google/', {
             code
         })
     }
 
-    static facebookAuth(code) {
+    static facebookRegister(code) {
         return api.post('/auth/facebook/', {
             code
         })
-    }    
+    }
+
+    static googleLogin(code) {
+        return api.post('/auth/google/login/', {
+            code
+        })
+    }
+
+    static facebookLogin(code) {
+        return api.post('/auth/facebook/login/', {
+            code
+        })
+    }
 }
